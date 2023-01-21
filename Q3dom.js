271e021683a04fa51cb5dcb8cc9d22c237611385
Afrid-Md <shaikmdafrid225>
2023-01-21 11:26:43 +0530

    pushing codes

--- Q3dom.js
+++
@@ -1,3 +0,0 @@
-var li=document.getElementsByTagName('li');
-li[4].textContent='Hello';
-console.log(li);
---
+++ Q4dom.js
@@ -0,0 +1,7 @@
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+
+secondItem.style.backgroundColor='green';
+
+var thirdItem = document.getElementsByClassName('list-group-item');
+
+thirdItem[2].remove();
